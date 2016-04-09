b13c558dff5a79e01406cdb9668fc835c6337a04
Ronaldo M. Rodrigues <ronaldo@whera.com.br>
2016-04-09 20:40:16 -0300

    add class product

---
+++ src/Resources/Products.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace WSW\LojaIntegrada\Resources;
+
+/**
+ * Class Products
+ * @package WSW\LojaIntegrada\Resources
+ * @author Ronaldo Matos Rodrigues <ronaldo@whera.com.br>
+ */
+class Products extends BaseResource
+{
+    /**
+     * @var string
+     */
+    protected $endPoint = '/v1/produto';
+}
