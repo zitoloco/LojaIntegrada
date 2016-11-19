62db5646015abcc896f3e3d958f272aa50529082
André Ribas <zitoloco@gmail.com>
2016-11-19 19:30:34 -0200

    add status resource

---
+++ src/Resources/Status.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WSW\LojaIntegrada\Resources;
+
+/**
+ * Class Statuses
+ *
+ * @package WSW\LojaIntegrada\Resources
+ * @author  Andre Ribas <ribas.andre@gmail.com>
+ */
+class Status extends BaseResource
+{
+    /**
+     * @var string
+     */
+    protected $endPoint = '/v1/situacao';
+}
